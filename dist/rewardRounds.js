882fb3cb7d39bcd407db0be8f682c84aaad17572
boid-com <john@Boid.com>
2022-02-04 02:31:50 -0600

    don't try to reward the current round

--- dist/rewardRounds.js
+++ dist/rewardRounds.js
@@ -10,6 +10,16 @@ let contract = 'pwrupnfts';
 if (env_1.default.chain == 'eos')
     contract = 'powerup.nfts';
 console.log('using contract:', contract);
+async function getRound() {
+    const configTbl = await eosio_1.getFullTable({ tableName: 'config', contract, type: nftTypes_1.Config }).catch(err => console.error(err));
+    const conf = configTbl[0];
+    const startTime = Date.parse(conf.start_time.toDate().toString()) / 1000;
+    const now = (Date.now() / 1000);
+    const elapsed = now - startTime;
+    const round = Math.floor(elapsed / conf.round_length_sec.toNumber()) + 1;
+    console.log("current Round: ", round);
+    return round;
+}
 async function claimAll() {
     const allClaimed = await eosio_1.getFullTable({ tableName: 'claimed', contract, type: nftTypes_1.Claimed });
     const unclaimed = allClaimed.filter(el => el.bronze_unclaimed.toNumber() > 0);
@@ -19,28 +29,36 @@ async function claimAll() {
         let data = nftTypes_1.Claim.from({ donator: row.account });
         const result = await eosio_1.doAction('claim', data, contract).catch(console.error);
         if (!result)
-            console.error('claim erorr');
-        else if (result.receipts.length == 0)
-            console.error('claim erorr:', result.errors);
-        if (result) {
-            console.log(result.receipts[0].url.toString());
-            console.log(result.receipts[0].receipt.id);
+            console.error('claim erorr', row);
+        else {
+            if (result.receipts.length == 0)
+                console.error('claim erorr:', result.errors);
+            else {
+                console.log(result.receipts[0].url.toString());
+                console.log(result.receipts[0].receipt.id);
+            }
         }
     }
 }
 async function init() {
     try {
         const rounds = await eosio_1.getFullTable({ tableName: 'rounds', contract: 'powerup.nfts', type: nftTypes_1.Rounds });
-        const unrewarded = rounds.filter(el => el.rewarded == false);
+        const currentRound = await getRound();
+        const unrewarded = rounds.filter(el => el.rewarded == false && el.id.toNumber() != currentRound);
+        console.log('found', unrewarded.length, 'unrewarded rounds');
         for (const round of unrewarded) {
             const data = nftTypes_1.Rewardround.from({ round_id: round.id });
             const result = await eosio_1.doAction('rewardround', data, contract).catch(console.error);
             if (!result)
-                return console.error('rewardround erorr');
-            if (result.receipts.length == 0)
-                console.error('rewardround erorr:', result.errors);
-            console.log(result.receipts[0].url.toString());
-            console.log(result.receipts[0].receipt.id);
+                console.error('rewardround erorr', round);
+            else {
+                if (result.receipts.length == 0)
+                    console.error('rewardround erorr:', result.errors);
+                else {
+                    console.log(result.receipts[0].url.toString());
+                    console.log(result.receipts[0].receipt.id);
+                }
+            }
         }
         await claimAll();
     }
--- src/rewardRounds.ts
+++ src/rewardRounds.ts
@@ -2,10 +2,22 @@ import env from './lib/env'
 import { getAccount, doAction, getFullTable, getResouceCosts, ResourceCosts, getAllScopes } from './lib/eosio'
 import ms from 'ms'
 import { shuffle } from './lib/utils'
-import { Claim, Claimed, Rewardround, Rounds } from './lib/types/nftTypes'
+import { Config, Claim, Claimed, Rewardround, Rounds } from './lib/types/nftTypes'
+
 let contract = 'pwrupnfts'
 if (env.chain == 'eos') contract = 'powerup.nfts'
 console.log('using contract:', contract);
+
+async function getRound(): Promise<number> {
+  const configTbl = await getFullTable({ tableName: 'config', contract, type: Config }).catch(err => console.error(err))
+  const conf: Config = configTbl[0]
+  const startTime = Date.parse(conf.start_time.toDate().toString()) / 1000
+  const now = (Date.now() / 1000)
+  const elapsed = now - startTime
+  const round = Math.floor(elapsed / conf.round_length_sec.toNumber()) + 1
+  console.log("current Round: ", round);
+  return round
+}
 
 async function claimAll() {
   const allClaimed: Claimed[] = await getFullTable({ tableName: 'claimed', contract, type: Claimed })
@@ -18,8 +30,10 @@ async function claimAll() {
     if (!result) console.error('claim erorr', row)
     else {
       if (result.receipts.length == 0) console.error('claim erorr:', result.errors)
-      console.log(result.receipts[0].url.toString())
-      console.log(result.receipts[0].receipt.id)
+      else {
+        console.log(result.receipts[0].url.toString())
+        console.log(result.receipts[0].receipt.id)
+      }
     }
   }
 }
@@ -27,15 +41,19 @@ async function claimAll() {
 async function init() {
   try {
     const rounds: Rounds[] = await getFullTable({ tableName: 'rounds', contract: 'powerup.nfts', type: Rounds })
-    const unrewarded = rounds.filter(el => el.rewarded == false)
+    const currentRound = await getRound()
+    const unrewarded = rounds.filter(el => el.rewarded == false && el.id.toNumber() != currentRound)
+    console.log('found', unrewarded.length, 'unrewarded rounds');
     for (const round of unrewarded) {
       const data = Rewardround.from({ round_id: round.id })
       const result = await doAction('rewardround', data, contract).catch(console.error)
       if (!result) console.error('rewardround erorr', round)
       else {
         if (result.receipts.length == 0) console.error('rewardround erorr:', result.errors)
-        console.log(result.receipts[0].url.toString())
-        console.log(result.receipts[0].receipt.id)
+        else {
+          console.log(result.receipts[0].url.toString())
+          console.log(result.receipts[0].receipt.id)
+        }
       }
     }
     await claimAll()
